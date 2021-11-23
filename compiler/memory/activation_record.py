fce2a96e854287ca3ec346daa6052ae51f402fc2
Isabellacnb <canalesisabella4@gmail.com>
2021-11-22 20:28:49 -0600

    :sparkles: Activation record impl

--- compiler/memory/activation_record.py
+++ compiler/memory/activation_record.py
@@ -1,3 +1,45 @@
+from structures import *
+
 class ActivationRecord:
-    def __init__(self):
-        pass
+    def __init__(self, memoryLimits):
+        self.parameters = []
+        self.localAddresses = memoryLimits[0]
+        self.temporaryAddresses = memoryLimits[1]
+        #TODO: parameters, call position, return address
+
+        self.size = 20000
+
+        self.localMemory = {}
+        for type in Type:
+            if type == Type.VOID:
+                continue
+            self.localAddresses[type] = (self.localAddresses[type] % self.size) * [None]
+        
+        self.temporaryMemory = {}
+        for type in Type:
+            if type == Type.VOID:
+                continue
+            self.temporaryMemory[type] = (self.temporaryMemory[type] % self.size) * [None]
+
+    def set(self, scope, address, value, addressType):
+        if (scope == Scope.TEMPORARY):
+            for type in Type:
+                if addressType == type:
+                    self.temporaryMemory[type.value][address] = value
+                    return
+        elif (scope == Scope.LOCAL):
+            for type in Type:
+                if addressType == type:
+                    self.localMemory[type.value][address] = value
+                    return
+
+    def get(self, scope, address, addressType):
+        if (scope == Scope.TEMPORARY):
+            for type in Type:
+                if addressType == type:
+                    return self.temporaryMemory[type.value][address]
+        elif (scope == Scope.LOCAL):
+            for type in Type:
+                if addressType == type:
+                    return self.localMemory[type.value][address]
+    
